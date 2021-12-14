9ded1ccd392def16dc4c766fc8ad87ac4082eddc
Thiha Thit <85206429+thihathit-opn@users.noreply.github.com>
2021-12-14 17:38:31 +0700

    let's try again buddy [skip ci]

--- release.config.js
+++ release.config.js
@@ -14,5 +14,6 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github"
-  ]
+  ],
+  preset: 'conventionalcommits'
 };
